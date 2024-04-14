53bca525837b3b6cb966c38902ba3920ee8a603d
Stefan <paunovski.stefan@gmail.com>
2024-04-14 21:33:38 +0200

    Excersice added number 3 and correction of file names

---
+++ Homework3/2.1.js
@@ -0,0 +1,10 @@
+let number = 0;
+if (number > 0) {
+    console.log ("The number is positive") ;  
+}
+if (number == 0) {
+    console.log ("The number is equal");
+}
+if (number < 0) {
+    console.log("The number is negative");
+}

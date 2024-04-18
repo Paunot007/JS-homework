e89b3deb41c12a3ac6071ea76fa218217756e8c0
Stefan <paunovski.stefan@gmail.com>
2024-04-18 13:17:40 +0200

    adding files

---
+++ Homework5/2.1.js
@@ -0,0 +1,8 @@
+let price= 34;
+if (price >= 100) {
+  console.log("You are eligible for 10$ coupon");
+} else if (price >= 50) {
+  console.log("You are eligible for 5$ coupon");
+} else if (price >= 0) {
+  console.log("Sorry you aren't eligible for coupons");
+}
---
+++ homework4/excercise.js
@@ -0,0 +1,8 @@
+let destination= USA;
+if (destination >= USA) {
+  console.log("You are eligible for 10$ coupon");
+} else if (destination >= Canada) {
+  console.log("You are eligible for 5$ coupon");
+} else if (destination >= UK) {
+  console.log("Sorry you aren't eligible for coupons");
+}
